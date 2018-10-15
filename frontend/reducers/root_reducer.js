7330c329d9f641f2fb66e3a4cd9b754b93e37f56
nakodila <elenatalk@gmail.com>
2018-10-15 17:07:27 +0200

    root reducer to set the state

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import entities from './entities_reducer';
+import errors from './errors_reducer';
+import session from './session_reducer';
+
+export default rootReducer = combineReducers ({
+  entities,
+  session,
+  errors
+});

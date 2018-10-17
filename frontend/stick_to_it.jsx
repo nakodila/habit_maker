a9e8278bf10bc7a3f3de6ec7713a0e8c16c4d842
nakodila <elenatalk@gmail.com>
2018-10-17 15:14:45 +0200

    bootstrap current user

--- frontend/stick_to_it.jsx
+++ frontend/stick_to_it.jsx
@@ -5,13 +5,22 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: {[window.currentUser.id]: window.currentUser }
+      },
+      session: {id: window.currentUser.id}
+    };
+
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
   window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.login = login;
-  window.signup = signup;
-
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
 });
